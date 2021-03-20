e98f20045e35b83b369062c61733d82296ee6d3c
Ronaldo L. Inocencio Jr <ronaldolopesjunior@yahoo.com.br>
2021-03-20 20:45:01 -0300

    #refactor: adicionar cors

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,11 @@
-import express, { Express, Router } from 'express'
 import { readdirSync } from 'fs'
+
+import express, { Express, Router } from 'express'
+import cors from 'cors'
 
 export const setupRoutes = (app: Express): void => {
   const router = Router()
+  app.use(cors())
   app.use(express.json())
   app.use('/api', router)
   readdirSync(`${__dirname.replace('config', 'routes')}`).map(async fileName => {
